61bbbd6a62804dc044783a2d44b2f9db78a0029a
CBraun <cbraun@gmx.li>
2015-12-07 15:02:39 +0100

    Logo Layout Edit im  Default View

--- FMA.View/Models/NotifyPropertyChangedBase.cs
+++ FMA.View/Models/NotifyPropertyChangedBase.cs
@@ -7,23 +7,23 @@ namespace FMA.View.Models
 {
     public class NotifyPropertyChangedBase : INotifyPropertyChanged
     {
-        private bool suspendRefreshPreview;
-        private bool missedEvents;
+        //private bool suspendRefreshPreview;
+        //private bool missedEvents;
 
         public void SuspendNotifyPropertyChanged()
         {
-            this.suspendRefreshPreview = true;
+      //      this.suspendRefreshPreview = true;
         }
 
         public void ResumeNotifyPropertyChanged(bool notifyMissedEvents = true,string propertyName = "")
         {
-            this.suspendRefreshPreview = false;
-            if (notifyMissedEvents && missedEvents)
-            {
-                    this.OnPropertyChanged(propertyName);
-            }
+            //this.suspendRefreshPreview = false;
+            //if (notifyMissedEvents && missedEvents)
+            //{
+            //        this.OnPropertyChanged(propertyName);
+            //}
 
-            missedEvents = false;
+            //missedEvents = false;
         }
 
 
@@ -32,11 +32,11 @@ namespace FMA.View.Models
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
-            if (suspendRefreshPreview)
-            {
-                missedEvents = true;
-                return;
-            }
+            //if (suspendRefreshPreview)
+            //{
+            //    missedEvents = true;
+            //    return;
+            //}
 
             var handler = PropertyChanged;
             if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
